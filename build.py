8f132ceb6a1a783d44e026e01c0f23312404ecf4
worse <oskar.rabo@gmail.com>
2025-09-29 07:11:21 +0200

    Added help and version flag handling, reordered stuff for better clarity

--- build.py
+++ build.py
@@ -1,28 +1,36 @@
 import os, sys
 from os.path import join
 from tools.Shell import Shell
-from tools.Output import Output
+from tools.Output import Colors, Output
 from tools.Build import Build
-from tools.Constants import C_BAD,CONFIG_FILE,SETUP_FILE,FORCE_ALT,FORCE_FLAG,SKIP_ALT,SKIP_FLAG,VERBOSE_ALT,VERBOSE_FLAG,C_EMPHASIS
+from tools.Constants import C_BAD, C_WARN,CONFIG_FILE, HELP_ALT, HELP_FLAG, RESET_ALT, RESET_FLAG,SETUP_FILE,FORCE_ALT,FORCE_FLAG,SKIP_ALT,SKIP_FLAG,VERBOSE_ALT,VERBOSE_FLAG,C_EMPHASIS, VERSION_ALT, VERSION_FLAG
+from tools.Usage import Usage
 
 # INIT
 
 argv = Shell.SplitFlags(sys.argv)
-
 config = Shell.ReadConfig(join("./", CONFIG_FILE))
-isFirstLaunch = not os.path.exists(join("./", SETUP_FILE))
 
+if (HELP_FLAG in argv or HELP_ALT in argv):
+	print(Usage)
+	exit(code=0)
+
+if (VERSION_FLAG in argv or VERSION_ALT in argv):
+	print(f"{C_EMPHASIS}Using {config["SDK_NAME"]} version {Colors.Yellow}{config["SDK_VERSION"]}{C_EMPHASIS} by worseuserr.{Colors.Reset}")
+	exit(code=0)
+
+isFirstLaunch = not os.path.exists(join("./", SETUP_FILE))
 force = FORCE_FLAG in argv or FORCE_ALT in argv
 skip = SKIP_FLAG in argv or SKIP_ALT in argv
+reset = RESET_FLAG in argv or RESET_ALT in argv
+shouldSetup = force or (isFirstLaunch and not skip)
 
-if (force and skip):
-	Output.Write(f"{C_BAD}Error: {SKIP_FLAG} ({SKIP_ALT}) and {FORCE_FLAG} ({FORCE_ALT}) cannot be passed simultaneously. Use --help for usage.")
+if ((force or reset) and skip):
+	Output.Write(f"{C_BAD}Error: {SKIP_FLAG} ({SKIP_ALT}) and {FORCE_FLAG if force else RESET_FLAG} ({FORCE_ALT if force else RESET_ALT}) cannot be passed simultaneously. Use --help for usage.")
 	exit(code=1)
 
-if (force or (isFirstLaunch and not skip)):
-	shouldSetup = True
-else:
-	shouldSetup = False
+if (force and reset):
+	Output.Write(f"{C_WARN}Note: --force-setup (-f) is unnecessary with --reset (-r)\n")
 
 if (VERBOSE_FLAG in argv or VERBOSE_ALT in argv):
 	Output.LogLevel = "verbose"
@@ -36,4 +44,3 @@ if (shouldSetup):
 	Build.Setup(config)
 sources = Build.GetSources(config)
 Build.Build(sources)
-
