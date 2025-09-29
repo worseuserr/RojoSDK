5fd2a2a108220ba0adc64fa99182ed4670e8d35b
worse <oskar.rabo@gmail.com>
2025-09-29 07:08:43 +0200

    Added usage info

---
+++ tools/Usage.py
@@ -0,0 +1,15 @@
+from tools.Output import Colors
+
+Usage = f"""
+{Colors.White}Usage:{Colors.Reset} python3 build.py {Colors.Italic}[OPTIONS]{Colors.Reset}
+
+{Colors.White}Description:{Colors.Reset}
+   This script builds all your libraries with your source code into /build.
+
+{Colors.White}Options:{Colors.Yellow}
+   -V, --version       Show the version and exit
+   -h, --help          Show this message and exit
+   -s, --skip-setup    Skip first-build setup (only use if you know what you are doing)
+   -f, --force-setup   Force first-build setup to run before building
+   -v, --verbose       Enable more verbose output
+   -r, --reset         Clean everything and rerun setup{Colors.Reset}"""
