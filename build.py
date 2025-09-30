fa03144e43c52861f6b7caea272dc58a58129982
worse <oskar.rabo@gmail.com>
2025-09-30 04:30:49 +0200

    Added clean flag compat check

--- build.py
+++ build.py
@@ -3,7 +3,7 @@ from os.path import join
 from tools.Shell import Shell
 from tools.Output import Colors, Output
 from tools.Build import Build
-from tools.Constants import C_BAD, C_WARN,CONFIG_FILE, HELP_ALT, HELP_FLAG, RESET_ALT, RESET_FLAG,SETUP_FILE,FORCE_ALT,FORCE_FLAG,SKIP_ALT,SKIP_FLAG,VERBOSE_ALT,VERBOSE_FLAG,C_EMPHASIS, VERSION_ALT, VERSION_FLAG
+from tools.Constants import C_BAD, C_WARN, CLEAN_ALT, CLEAN_FLAG,CONFIG_FILE, HELP_ALT, HELP_FLAG, RESET_ALT, RESET_FLAG,SETUP_FILE,FORCE_ALT,FORCE_FLAG,SKIP_ALT,SKIP_FLAG,VERBOSE_ALT,VERBOSE_FLAG,C_EMPHASIS, VERSION_ALT, VERSION_FLAG
 from tools.Usage import Usage
 
 # INIT
@@ -25,12 +25,18 @@ skip = SKIP_FLAG in argv or SKIP_ALT in argv
 reset = RESET_FLAG in argv or RESET_ALT in argv
 shouldSetup = (force or reset) or (isFirstLaunch and not skip)
 
+# OPTION COMPATS
+
 if ((force or reset) and skip):
-	Output.Write(f"{C_BAD}Error: {SKIP_FLAG} ({SKIP_ALT}) and {FORCE_FLAG if force else RESET_FLAG} ({FORCE_ALT if force else RESET_ALT}) cannot be passed simultaneously. Use --help for usage.")
+	Output.Write(f"{C_BAD}Error: {SKIP_FLAG} ({SKIP_ALT}) and {FORCE_FLAG if force else RESET_FLAG} ({FORCE_ALT if force else RESET_ALT}) cannot be passed simultaneously. Use {HELP_FLAG} or {HELP_ALT} for usage.")
 	exit(code=1)
 
+if (CLEAN_FLAG in argv or CLEAN_ALT in argv):
+	if (force or reset or skip):
+		Output.Write(f"{C_WARN}Warn: {CLEAN_FLAG} ({CLEAN_ALT}) ignores other flags. Use {HELP_FLAG} or {HELP_ALT} for usage.")
+
 if (force and reset):
-	Output.Write(f"{C_WARN}Note: --force-setup (-f) is unnecessary with --reset (-r)\n")
+	Output.Write(f"{C_WARN}Warn: {FORCE_FLAG} ({FORCE_ALT}) is unnecessary with {RESET_FLAG} ({RESET_ALT}) Use {HELP_FLAG} or {HELP_ALT} for usage.\n")
 
 if (VERBOSE_FLAG in argv or VERBOSE_ALT in argv):
 	Output.LogLevel = "verbose"
