dc0e93790e155fa567b983632cc812cc9279c874
worse <oskar.rabo@gmail.com>
2025-10-04 13:13:33 +0200

    Add more tests

--- tests/cli.py
+++ tests/cli.py
@@ -5,8 +5,16 @@ def Test():
 	q = cli.AddArg("--quiet", "-q", "Quiet")
 	v = cli.AddArg("--verbose", "-v", "Verbose")
 	f = cli.AddArg("--force", "-f", "Force")
+	u = cli.AddArg("--unused", "-u", "Unused")
+	fc = cli.AddArg("--fclean", "-F", "Fclean")
+	r = cli.AddArg("--reset", "-r", "Reset")
+	ver = cli.AddExclusiveArg("--version", "-V", "Version")
 
-	cli.AddGroup(Conflict.Error, q, v)
+	# cli.AddGroup(Conflict.Error, q, v)
+	cli.AddGroup(Conflict.Overlap, fc, r)
 
-	result = cli.Parse("--quiet", "-v", "--force")
+	result = cli.Parse(["test.py", "--quiet", "-v", "--force", "--reset", "-F"])
 	print(result)
+	result = cli.Parse(["test.py", "--quiet", "-v", "--force", "-V", "--reset", "-F"])
+	print(result)
+
